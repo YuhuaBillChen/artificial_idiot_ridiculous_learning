4a1390196f65adf04d92eedbe29f1dc1c44ca1bd
Yuhua Bill Chen <yuhuabilL@gmail.com>
2020-04-01 12:29:51 -0400

    Updated ddpg

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -8,6 +8,7 @@ import numpy as np
 import gym
 import random
 from collections import deque
+import tensorflow.keras as keras
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Concatenate, Input
@@ -19,16 +20,18 @@ SAVE_PER_EPISODE = 100
 NB_EPISDOE = 500
 LEN_EPISODE = 999
 TRAINING_SUCCESS_CNT = 5
-TEST_PER_EPISODE = 50
+TEST_PER_EPISODE = 10
 FN_PREFIX = "DDPG-MCC"
 GYM_ENVIRON = 'MountainCarContinuous-v0'
 WIN_REWARD = 90
+
 
 class OUNoise:
     """
         OU Noise
         Credit from https://github.com/floodsung/DDPG/blob/master/ou_noise.py
     """
+
     def __init__(self, action_dimension, mu=0, theta=0.15, sigma=0.2):
         self.action_dimension = action_dimension
         self.mu = mu
@@ -52,15 +55,15 @@ class DDPGAgent(object):
         DQN Agent
     """
     MEMORY_LENGTH = 20000
-    BATCH_SIZE = 64
-    TARGET_UPDATE_TAU = 0.1
-    ACTION_SCALE = 2.0
+    BATCH_SIZE = 32
+    TARGET_UPDATE_TAU = 0.01
+    ACTION_BOUNDARY = 2.0
 
     def __init__(
             self,
             env,
             memory=deque(maxlen=MEMORY_LENGTH),
-            gamma=0.999,
+            gamma=0.99,
             learning_rate=1e-3,
             epsilon_init=1.0,
             epsilon_min=0.1,
@@ -101,10 +104,10 @@ class DDPGAgent(object):
 
     def update_target(self):
         actor_weights = self.TARGET_UPDATE_TAU * np.array(self.actor_model.get_weights()) + \
-                        (1-self.TARGET_UPDATE_TAU) * np.array(self.actor_target.get_weights())
+                        (1 - self.TARGET_UPDATE_TAU) * np.array(self.actor_target.get_weights())
         self.actor_target.set_weights(actor_weights)
         critic_weights = self.TARGET_UPDATE_TAU * np.array(self.critic_model.get_weights()) + \
-                         (1-self.TARGET_UPDATE_TAU) * np.array(self.critic_target.get_weights())
+                         (1 - self.TARGET_UPDATE_TAU) * np.array(self.critic_target.get_weights())
 
         self.critic_target.set_weights(critic_weights)
 
@@ -114,9 +117,14 @@ class DDPGAgent(object):
         input_layer = Input(shape=state_shape)
         hidden_layer1 = Dense(24, activation="relu")(input_layer)
         hidden_layer2 = Dense(48, activation="relu")(hidden_layer1)
-        results = Dense(self.env.action_space.shape[0], activation='tanh')(hidden_layer2)
+        results = Dense(
+            self.env.action_space.shape[0],
+            activation='tanh',
+            kernel_initializer=keras.initializers.RandomUniform(minval=-0.03, maxval=0.03)
+        )(hidden_layer2)
+        results = self.ACTION_BOUNDARY*results
         model = Model(inputs=input_layer, outputs=results)
-        return model    # Probs of action
+        return model  # Probs of action
 
     def nn_critic_model(self, actor_model):
         # Critic
@@ -131,7 +139,7 @@ class DDPGAgent(object):
 
     def make_actor_train_fn(self):
         upd_grad = tf.gradients(
-            self.merged_model.output,
+            -self.merged_model.output,
             self.actor_model.trainable_weights
         )
 
@@ -143,7 +151,7 @@ class DDPGAgent(object):
             inputs=[
                 self.actor_model.input
             ],
-            outputs=[self.actor_model.output],
+            outputs=[self.actor_model.output, upd_grad],
             updates=update_op
         )
         return train_fn
@@ -157,7 +165,7 @@ class DDPGAgent(object):
 
         # Mean squared error of the prediction
         loss = kb.mean(rewards_pl + (1.0 - dones_pl) * self.gamma * new_q_hats - q_hats)
-        loss = kb.mean(loss**2)
+        loss = kb.mean(loss ** 2)
 
         adam = Adam(lr=self.learning_rate)
 
@@ -205,12 +213,12 @@ class DDPGAgent(object):
         c_output, crt_loss = self.critic_train_fn(
             [states, new_states, rewards, dones])
 
-        _ = self.actor_train_fn([states])
+        _, upd_grad = self.actor_train_fn([states])
 
         return crt_loss
 
     def policy_action(self, state):
-        action_val = self.ACTION_SCALE*self.take_action(state)
+        action_val = self.take_action(state)
         action_val += self.exploration_noise.noise()
         return action_val
 
@@ -227,27 +235,30 @@ class DDPGAgent(object):
         self.actor_model = load_model(fn.replace('.model', '-actor.model'))
         self.critic_model = load_model(fn.replace('.model', '-critic.model'))
 
-def evaluation(env, agent, i_episode):
+
+def evaluation(env, agent):
     # Test during training
-    if i_episode % TEST_PER_EPISODE == 0:
-        for _ in range(TRAINING_SUCCESS_CNT):
-            print("Testing.. %d/%d" % (_, TRAINING_SUCCESS_CNT))
-            cur_state = env.reset().reshape(1, -1)
-            max_distance = -1.5
-            for step in range(LEN_EPISODE):
-                action = agent.take_action(cur_state)
-                new_state, reward, done, _ = env.step(action)
-                env.render()
-                new_state = new_state.reshape(1, -1)
-                cur_state = new_state
-                max_distance = max(new_state[0][0], max_distance)
-                if done:
-                    break
-            if reward > WIN_REWARD:
-                print("Testing completed, in %d steps, reward: %f." % (step, reward))
-            else:
-                print("Testing failed, reward: %.2f, maxdist: %.2f" % (reward, max_distance))
+    for _ in range(TRAINING_SUCCESS_CNT):
+        print("Testing.. %d/%d" % (_, TRAINING_SUCCESS_CNT))
+        cur_state = env.reset().reshape(1, -1)
+        max_distance = -1.5
+        for step in range(LEN_EPISODE):
+            action = agent.take_action(cur_state)
+            if agent.time_step > 10000:
+                print(action)
+            new_state, reward, done, _ = env.step(action)
+            env.render()
+            new_state = new_state.reshape(1, -1)
+            cur_state = new_state
+            max_distance = max(new_state[0][0], max_distance)
+            if done:
                 break
+        if reward > WIN_REWARD:
+            print("Testing completed, in %d steps, reward: %f." % (step, reward))
+        else:
+            print("Testing failed, reward: %.2f, maxdist: %.2f" % (reward, max_distance))
+            return False
+    return True
 
 
 def train():
@@ -262,6 +273,7 @@ def train():
     for i_episode in range(nb_episode):
         max_distance = -1.5
         cur_state = env.reset().reshape(1, -1)
+        agent.exploration_noise.reset()
 
         for step in range(len_episode):
             action = agent.policy_action(cur_state)
@@ -283,9 +295,6 @@ def train():
 
             cur_state = new_state
 
-        agent.exploration_noise.reset()
-        evaluation(agent=agent, env=env, i_episode=i_episode)
-
         if reward > WIN_REWARD:
             print("Completed in %d episode, in %d steps, reward: %f." % (i_episode, step, reward))
         else:
@@ -294,6 +303,10 @@ def train():
 
         if i_episode % SAVE_PER_EPISODE == 0:
             agent.save_model("{}-episode-{}.model".format(FN_PREFIX, i_episode))
+
+        if i_episode % TEST_PER_EPISODE == 0:
+            if evaluation(agent=agent, env=env):
+                break
 
     agent.save_model("{}-episode-{}.model".format(FN_PREFIX, i_episode))
     env.close()
@@ -305,7 +318,7 @@ def test():
     len_episode = 200
 
     agent = DDPGAgent(env=env)
-    chkpts = sorted(glob.glob("%s-episode-*.model"%FN_PREFIX), key=os.path.basename, reverse=True)
+    chkpts = sorted(glob.glob("%s-episode-*.model" % FN_PREFIX), key=os.path.basename, reverse=True)
     agent.load_model(chkpts[0].replace("-actor.model", ".model").replace("-critic.model", ".model"))
     print("loaded checkpoint:%s" % chkpts[0])
 
